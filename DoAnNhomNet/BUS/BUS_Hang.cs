1aba96e313904741072bb37678bb2547081bd186
ThoGtb <ngothithunc2004@gmail.com>
2024-04-06 22:27:46 +0700

    ban thay doi lan2

---
+++ DoAnNhomNet/BUS/BUS_Hang.cs
@@ -0,0 +1,38 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUS
+{
+    public class BUS_Hang
+    {
+
+        private static BUS_Hang instance;
+
+        public static BUS_Hang Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BUS_Hang();
+                }
+                return instance;
+            }
+        }
+        public BUS_Hang() { }
+        public void Xem(DataGridView data)
+        {
+            data.DataSource = DAO_Hang.Instance.Xem();
+        }
+        public void Them(Hang hang)
+        {
+            DAO_Hang.Instance.Them(hang);
+        }
+    }
+}
---
+++ DoAnNhomNet/DoAnNhomNet/FormDanhMucHang.cs
@@ -0,0 +1,54 @@
+﻿using BUS;
+using DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnNhomNet
+{
+    public partial class FormDanhMucHang : Form
+    {
+        BUS_Hang busHang = new BUS_Hang();
+        public FormDanhMucHang()
+        {
+            InitializeComponent();
+            LoadForm();
+        }
+        public void LoadForm()
+        {
+            BUS_Hang.Instance.Xem(dgvHang);
+        }
+
+        private void dgvHang_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = e.RowIndex;
+            txtMaHang.Text = dgvHang.Rows[row].Cells[0].Value.ToString();
+            txtTenHang.Text = dgvHang.Rows[row].Cells[1].Value.ToString();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Hang h = new Hang
+                {
+                    MaHang = txtMaHang.Text,
+                    TenHang = txtTenHang.Text,
+                };
+                busHang.Them(h);
+                MessageBox.Show("Them thanh cong");
+                BUS_Hang.Instance.Xem(dgvHang);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Them khong thanh cong");
+            }
+        }
+    }
+}
