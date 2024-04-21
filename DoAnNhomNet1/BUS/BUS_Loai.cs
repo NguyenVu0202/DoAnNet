188812ba2cc97c2975350ef35525f4d95fabd110
ThoGtb <ngothithunc2004@gmail.com>
2024-04-21 23:09:49 +0700

    thay doi lan3

---
+++ DoAnNhomNet1/BUS/BUS_Loai.cs
@@ -0,0 +1,45 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUS
+{
+    public class BUS_Loai
+    {
+        private static BUS_Loai instance;
+
+        public static BUS_Loai Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BUS_Loai();
+                }
+                return instance;
+            }
+        }
+        public BUS_Loai() { }
+        public void Xem(DataGridView data)
+        {
+            data.DataSource = DAO_Loai.Instance.Xem();
+        }
+        public void Them(Loai loai)
+        {
+            DAO_Loai.Instance.Them(loai);
+        }
+        public void Xoa(TextBox loai)
+        {
+            DAO_Loai.Instance.Xoa(loai);
+        }
+        public bool Sua(string maLoai, Loai loai)
+        {
+            return DAO_Loai.Instance.Sua(maLoai,loai);
+        }
+    }
+}
